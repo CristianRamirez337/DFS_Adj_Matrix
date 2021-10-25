f27d6fbf86d23d1a87144034c11b80a3642704e1
CristianRamirez337 <37085685+CristianRamirez337@users.noreply.github.com>
2021-10-24 21:11:45 -0500

    Pretty graph

--- DFSmatrix.py
+++ DFSmatrix.py
@@ -1,6 +1,9 @@
 from pyvis.network import Network
 
-# Python3 implementation of the approach 
+'''
+Cristian Aurelio Ramírez Anzaldo    A01066337
+Algorithms
+'''
 class Graph:
       
     adjacency = []
@@ -32,40 +35,34 @@ class Graph:
         # For every node of the graph
         for i in range(self.v):
               
-            # If some node is adjacent to the 
-            # current node and it has not 
-            # already been visited
+            # check if there is an unvisited node there
             if (Graph.adjacency[start][i] == 1 and
                     (not visited[i])):
                 self.DFS(i, visited)
   
 
-v, e = 5, 4
+v, e = 10, 14
   
 network = Network()
 
 # Create the graph
 G = Graph(v, e)
 
-nodes = [0, 1, 2, 3, 4]
+nodes = [j for j in range(10)]
 network.add_nodes(nodes)
 
-for i in nodes:
+for i in nodes[1:]:
     G.addEdge(0, i)
     network.add_edge(0, i, weight=.87)
 
+G.addEdge(5,2)
+network.add_edge(5, 2, weight=.87)
 
 network.toggle_physics(True)
 network.show('mygraph.html')
 network.show_buttons(filter_=['physics'])
   
-# Visited vector to so that a vertex
-# is not visited more than once
-# Initializing the vector to false as no
-# vertex is visited at the beginning
 visited = [False] * v
   
-# Perform DFS
-G.DFS(0, visited);
-  
-# This code is contributed by ng24_7
+# DFS
+G.DFS(0, visited) 
